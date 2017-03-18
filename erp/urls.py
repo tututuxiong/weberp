e2ee70cba0399e946eba851b1c9696277ab5a2ee
alan zha <yongmao.zha@gmail.com>
2017-03-18 10:15:20 +0800

    fix reflesh problem for order and order/1

--- erp/urls.py
+++ erp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'orders$', views.index, name='index'),
+    url(r'order/[0-9]{1,4}$', views.index, name='index'),
 ]
