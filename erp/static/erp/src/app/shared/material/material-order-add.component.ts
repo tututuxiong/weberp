7d6bbe6843f64ba22c096a72707cec63102ddcf8
LegendOfFire <seu.liangf@gmail.com>
2017-03-29 15:59:28 +0800

    Data flow implementation in modal.

--- erp/static/erp/src/app/shared/material/material-order-add.component.ts
+++ erp/static/erp/src/app/shared/material/material-order-add.component.ts
@@ -1,29 +1,20 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { MaterialOrder } from './material-order';
 
 @Component({
   selector: 'ngbd-modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <p>Hello, {{name}}!</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" (click)="activeModal.close('Close click')">Close</button>
-    </div>
-  `
+  moduleId: module.id,
+  templateUrl: './templates/ngbd-modal-content.html'
 })
-export class NgbdModalContent {
-  @Input()
-  name: string;
 
-  constructor(public activeModal: NgbActiveModal) {}
+export class NgbdModalContent {
+    @Input()
+    materialOrder: MaterialOrder;
+
+    constructor(public activeModal: NgbActiveModal) {}
 }
 
 @Component({
@@ -35,8 +26,18 @@ export class NgbdModalContent {
 export class MaterialOrderAddComponent {
   constructor(private modalService: NgbModal) {}
 
+  @Input()
+  materialOrderList: MaterialOrder[];
+
+  @Input()
+    orderId: number;
+
+  newMaterialOrder: MaterialOrder;
+
+
   open() {
     const modalRef = this.modalService.open(NgbdModalContent);
-    modalRef.componentInstance.name = 'World';
+    this.newMaterialOrder = new MaterialOrder(this.orderId);
+    modalRef.componentInstance.materialOrder = this.newMaterialOrder;
   }
 }
--- erp/static/erp/src/app/shared/material/material-order.ts
+++ erp/static/erp/src/app/shared/material/material-order.ts
@@ -11,4 +11,17 @@ export class MaterialOrder {
     subOrderCount: number;
     materialSubOrderInfoList: MaterialSubOrder[];
     modifyMode: Boolean;
+
+    constructor(parentOrderId: number) {
+        this.id = -1;
+        this.orderId = parentOrderId;
+        this.name = 'New Material Order';
+        this.date = 'today';
+        this.price = 100;
+        this.comment = '';
+        this.status = 'new';
+        this.subOrderCount = 0;
+        this.materialSubOrderInfoList = [];
+        this.modifyMode = false;
+    }
 }
