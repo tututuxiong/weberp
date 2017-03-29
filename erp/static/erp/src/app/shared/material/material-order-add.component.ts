e0583fac2c4b33adc4f3a6bf8c73e564b56df74d
LegendOfFire <seu.liangf@gmail.com>
2017-03-29 15:31:56 +0800

    Add Modal functionality.

---
+++ erp/static/erp/src/app/shared/material/material-order-add.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input} from '@angular/core';
+
+import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Hi there!</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>Hello, {{name}}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+export class NgbdModalContent {
+  @Input()
+  name: string;
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
+
+@Component({
+  selector: 'new-material-order',
+  moduleId: module.id,
+  templateUrl: './templates/material-order-add.html'
+})
+
+export class MaterialOrderAddComponent {
+  constructor(private modalService: NgbModal) {}
+
+  open() {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = 'World';
+  }
+}
--- erp/static/erp/src/app/shared/shared.module.ts
+++ erp/static/erp/src/app/shared/shared.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { MaterialOrder } from './material/material-order';
 import { MaterialOrderComponent } from './material/material-order.component';
+import { NgbdModalContent, MaterialOrderAddComponent } from './material/material-order-add.component';
 import { MaterialSubOrderComponent } from './material/material-sub-order.component';
 import { MaterialSubOrderEditableComponent } from './material/material-sub-order-editable.component';
 
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        NgbModule.forRoot()
     ],
 
     declarations: [
          MaterialOrderComponent,
+         MaterialOrderAddComponent,
          MaterialSubOrderComponent,
-         MaterialSubOrderEditableComponent
+         MaterialSubOrderEditableComponent,
+         NgbdModalContent
+    ],
+
+    entryComponents: [
+        NgbdModalContent
     ],
 
     exports: [
          MaterialOrderComponent,
+         MaterialOrderAddComponent,
          MaterialSubOrderComponent,
          MaterialSubOrderEditableComponent
     ]
