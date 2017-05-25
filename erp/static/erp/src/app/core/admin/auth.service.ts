213517db13770ee30ce9879988fa1e9da8c3dd88
eyoozha <alan.zha@ericsson.com>
2017-05-25 15:46:48 +0800

    compile bug fix

--- erp/static/erp/src/app/core/admin/auth.service.ts
+++ erp/static/erp/src/app/core/admin/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Headers, Http, Response } from "@angular/http";
 
-import { Observable } from "rxjs/observable";
+import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
