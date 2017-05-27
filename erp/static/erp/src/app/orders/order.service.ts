58c7e6ce7a64369e781d18d088b93f4d9dacc680
eyoozha <alan.zha@ericsson.com>
2017-05-27 16:50:55 +0800

    update bootstap

--- erp/static/erp/src/app/orders/order.service.ts
+++ erp/static/erp/src/app/orders/order.service.ts
@@ -10,12 +10,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class OrderService {
   //  private ordersUrl = 'api/Orders';  // URL to web api
-    private ordersUrl = 'app/Orders';  // URL to web api
-    private headers = new Headers({'Content-Type': 'application/json'});
+  private ordersUrl = 'app/Orders';  // URL to web api
+  private headers = new Headers({ 'Content-Type': 'application/json' });
 
-    constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
-  private handleError (error: Response | any) {
+  private handleError(error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
@@ -29,38 +29,38 @@ export class OrderService {
     return Observable.throw(errMsg);
   }
 
-    private extractOrderListInfoData(res: Response) {
-             let body = res.json();
-             return body.orderInfoList || { };
-    }
+  private extractOrderListInfoData(res: Response) {
+    let body = res.json();
+    return body.orderInfoList || {};
+  }
 
-    private extractOrderInfoData(res: Response) {
-             let body = res.json();
-             return body;
-    }
+  private extractOrderInfoData(res: Response) {
+    let body = res.json();
+    return body;
+  }
 
 
-    getOrders() : Observable<Order[]> {
-      console.log("get orders.!");
-        return this.http.get(this.ordersUrl)
-                        .map(this.extractOrderListInfoData)
-                        .catch(this.handleError);
-    }
+  getOrders(): Observable<Order[]> {
+    console.log("get orders.!");
+    return this.http.get(this.ordersUrl)
+      .map(this.extractOrderListInfoData)
+      .catch(this.handleError);
+  }
 
-    getOrder(id: number | string) : Observable<Order> {
-        const url = `${this.ordersUrl}/${id}`;
+  getOrder(id: number | string): Observable<Order> {
+    const url = `${this.ordersUrl}/${id}`;
 
-        return this.http.get(url)
-                        .map(this.extractOrderInfoData)
-                        .catch(this.handleError);
-    };
+    return this.http.get(url)
+      .map(this.extractOrderInfoData)
+      .catch(this.handleError);
+  };
 
-    addOrder(order: Order) : Observable<Order> {
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-      const url = `${this.ordersUrl}/0`;
-      return  this.http.put(url, { order }, options)
-                       .map(this.extractOrderInfoData)
-                       .catch(this.handleError);
-    }
+  addOrder(order: Order): Observable<Order> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    const url = `${this.ordersUrl}/0`;
+    return this.http.put(url, { order }, options)
+      .map(this.extractOrderInfoData)
+      .catch(this.handleError);
+  }
 }
